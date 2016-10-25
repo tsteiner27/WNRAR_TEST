b5763fcd8d484f32c52b98e81268d020325b4180
WAKELY\tyler.steiner <tylersteinersoccer@gmail.com>
2016-10-25 14:50:08 -0600

    Added comments

--- Github Test.sql
+++ Github Test.sql
@@ -1,5 +1,13 @@
+------------------------------------------------------------------------------------
+-- This code is to test the use of Github and Source Tree as a form of source
+-- control for all of WNRAR
+-- Author: Tyler 10/25/2016
+------------------------------------------------------------------------------------
 
 SELECT Number * 20, Test + 'ing', Other
 FROM (
 	 SELECT 5 * 4 AS [Number], 'Test' AS [Test], NULL AS [Other]
 	 ) t1
+
+
+
